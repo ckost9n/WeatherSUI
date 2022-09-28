bc8a3442519e87f1003d8cd34a7f3ffabb3d5354
ckost9n <ckost9n@gmail.com>
2022-09-28 18:36:30 +0300

    Initial Commit

---
+++ WeatherSUI/WeatherSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSUIApp.swift
+//  WeatherSUI
+//
+//  Created by Konstantin on 28.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
