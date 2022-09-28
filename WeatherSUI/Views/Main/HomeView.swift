56380a4267c5c759725beb22d104e18bcc14b529
ckost9n <ckost9n@gmail.com>
2022-09-28 21:30:01 +0300

    create attributedString

--- WeatherSUI/Views/Main/HomeView.swift
+++ WeatherSUI/Views/Main/HomeView.swift
@@ -24,20 +24,39 @@ struct HomeView: View {
                 .frame(maxHeight: .infinity, alignment: .top)
                 .padding(.top, 257)
             
-            VStack {
+            VStack(spacing: -10) {
                 Text("Montreal")
                     .font(.largeTitle)
                 
                 VStack {
-                    Text("19°" + "\n" + "Mostly Clear")
+                    Text(attributedString)
                     
                     Text("H:24°   L:18°")
                         .font(.title3.weight(.semibold))
+                    
                 }
                 Spacer()
             }
             .padding(.top, 51)
         }
+    }
+    
+    private var attributedString: AttributedString {
+        var string = AttributedString("19°" + "\n " + "Mostly Clear")
+        
+        guard let temp = string.range(of: "19°") else { return string }
+        string[temp].font = .system(size: 96, weight: .thin)
+        string[temp].foregroundColor = .primary
+        
+        guard let pipe = string.range(of: " | ") else { return string }
+        string[pipe].font = .title3.weight(.semibold)
+        string[pipe].foregroundColor = .secondary
+        
+        guard let weather = string.range(of: "Mostly Clear") else { return string }
+        string[weather].font = .title3.weight(.semibold)
+        string[weather].foregroundColor = .secondary
+        
+        return string
     }
 }
 
